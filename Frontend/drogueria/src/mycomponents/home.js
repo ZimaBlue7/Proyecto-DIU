7c7e68830866a2250b3cd0c34a6ce33114f45ed3
Juan David Olaya <marteslosamo@gmail.com>
2022-05-13 09:02:31 -0500

    1.0

---
+++ Frontend/drogueria/src/mycomponents/home.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    
+        return (
+          <>
+            <header className='header1'>
+                help 
+                </>
+          </>
+        );
+      
+}
+
+export default App;
