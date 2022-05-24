c23d278cbc6ed0f6305ba7fdd7ef76c1beac9a2d
Juan David Olaya <marteslosamo@gmail.com>
2022-05-24 11:15:59 -0500

    1.2

--- Frontend/drogueria/src/components/Home/home.js
+++ Frontend/drogueria/src/components/Home/home.js
@@ -1,17 +1,25 @@
 import React, { Component, useState, useContext, useEffect } from "react";
 import { styled, useTheme } from "@mui/material/styles";
-import '../mycomponents/home.css';
+import '../../Styles/home.css';
 import Container from "@mui/material/Container";
 
 export default function home(){ 
     
     return (
-          <Container>
-            <section className='cuadro'> 
-            
+          <Container className="body">
+
+            <section className="barraArriba">
+              {/* Direciones e ingreso al perfil del usuario */}
             </section>
 
-            <p>hola </p>
+            <section className="Imagen1">
+
+            </section>
+
+            <section className='cuadro'> 
+            {/* Cuatro secciones (ultimos productos, productos,acerca de..) */}
+            </section>
+
           </Container>
         );
       
