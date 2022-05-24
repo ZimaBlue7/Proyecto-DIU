248d8a17e1e8d49a6d0496c5ba22c0da5e11d796
Juan David Olaya <marteslosamo@gmail.com>
2022-05-24 10:48:41 -0500

    1.1
    Carpetas

---
+++ Frontend/drogueria/src/components/Home/home.js
@@ -0,0 +1,19 @@
+import React, { Component, useState, useContext, useEffect } from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import '../mycomponents/home.css';
+import Container from "@mui/material/Container";
+
+export default function home(){ 
+    
+    return (
+          <Container>
+            <section className='cuadro'> 
+            
+            </section>
+
+            <p>hola </p>
+          </Container>
+        );
+      
+}
+
--- Frontend/drogueria/src/index.js
+++ Frontend/drogueria/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import home from './mycomponents/home.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <home />
+    <App/>
   </React.StrictMode>
 );
 
