2e37cdf25c15b04ccaa23613280129e64e98bbd5
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-07-08 11:31:09 -0500

    1.2

---
+++ Frontend/drogueria/src/components/Home/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../../Styles/Footer.css';
+
+
+
+function Footer() {
+  return (
+    <div>
+   <footer>
+        
+        <ul class="footer-left">
+            
+            <li>
+                <a>Publicidad</a>
+            </li>
+            <li>
+                <a>Negocios</a>
+            </li>
+            <li>
+                <a>Cómo funciona la Búsqueda</a>
+            </li>
+        </ul>
+        <ul class="footer-center">
+            <li>
+                <a>Centro 1</a>
+            </li>
+            <li>
+                <a >Centro 1</a>
+            </li>
+            <li>
+                <a >Centro 1</a>
+            </li>
+        </ul>
+        <ul class="footer-right">
+            <li>
+                <a >Privacidad</a>
+            </li>
+            <li>
+                <a >Condiciones</a>
+            </li>
+            <li>
+                <a >Preferencias</a>
+            </li>
+        </ul>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Frontend/drogueria/src/components/Home/Login.jsx
@@ -0,0 +1,134 @@
+import React, {useState,useContext,useEffect} from 'react'
+import Swal from 'sweetalert2';
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import '../../Styles/Login.css';
+import { createContext } from "react";
+const Login = () => {
+    
+
+    const UserContext = createContext()
+    const userContext = useContext(UserContext);
+    const {darDatos, verificarAutenticada} = userContext;
+
+  const navigate = useNavigate();
+
+  const [body, setBody] = useState({ usuario: '', password: '' })
+  
+
+  useEffect(() => {
+
+      const elem = window.localStorage.getItem('usuario')
+      const dato = elem ? JSON.parse(elem) : null
+
+      if(dato){
+          verificarAutenticada();
+          navigate("/dashboard");
+          return <></>
+      }
+
+  }, [])
+  
+  const handleChange = (e) => {
+      setBody({...body,[e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = async (e) =>{
+
+      e.preventDefault();
+
+      try{
+
+          const res = await axios.post('', body); 
+          console.log(res.data[0])
+          if( res.data.length > 0 ){
+
+              Swal.fire({
+                  title: 'Verificando informacion',
+                  timer: 1000,
+                  timerProgressBar: true,
+                  didOpen: () => {
+                      Swal.showLoading()
+                  }
+              }).then(() => {
+
+                  Swal.fire({
+                      icon: 'success',
+                      title: 'Bienvenido a SucurSalud '+ res.data[0].nombre,
+                      showConfirmButton: false,
+                      timer: 3000,
+                  }).then(function() {
+                      darDatos(res.data[0])
+                      navigate("/dashboard");
+                  });
+                      
+              })
+              
+          }
+          else{
+
+              Swal.fire({
+                  title: 'Verificando informacion',
+                  timer: 1000,
+                  timerProgressBar: true,
+                  didOpen: () => {
+                      Swal.showLoading()
+                  }
+              }).then(() => {
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'No estás registrado',
+                      showConfirmButton: false,
+                      timer: 2000,
+                  });
+              })
+              
+          }
+                  
+
+      }catch(e){
+          console.log(e)
+      }
+      
+  };
+
+    return (
+        <div>
+             <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300&display=swap" rel="stylesheet"></link>
+   <body className='body'>
+   <header className="header">
+       
+    </header>
+    <main>
+
+        
+    <section className="Login">
+   
+    <section className="Login__container">
+    <h2 className="login">Inicia sesion</h2>
+       <div className="profile icon"></div>
+       <div className="profile icon2"></div>
+       <div className="arrow-right icon"></div>
+       <div className="mail icon"></div>
+       <div className="key2 icon"></div>
+       <form className="Login__container--form" onSubmit={handleSubmit}>
+            <input name='email'  onChange={handleChange} className="input" type="email" placeholder="Correo" required></input>
+            <input name='password_'  onChange={handleChange} className="input" type="password" placeholder="Contraseña" required></input>
+            <button className="button">Iniciar sesion</button>
+       </form>
+       
+
+    </section>
+    <section className="Registrate">
+        <a>¿No tienes cuenta?</a>
+        <button className="button-register"><a href='/register'>Registrarme</a></button>
+    </section>
+    </section>
+    </main>
+   </body>
+
+        </div>
+    )
+}
+
+export default Login
